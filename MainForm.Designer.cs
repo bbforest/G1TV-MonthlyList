eeb728a70e9e3a51b8a50a3f3093d0fdb549709b
에케EKE <eke@bbforest.net>
2022-02-06 01:54:41 +0900

    feat: Change web client, add the search function
    Changed the web client.
    NSoupClient -> WebClient
    Added a word search function.
    웹 클라이언트를 변경했어요.
    NSoupClient -> WebClient
    단어 검색 기능을 추가했어요.

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -38,7 +38,8 @@ namespace G1방송기사월간검색
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.search = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -51,10 +52,10 @@ namespace G1방송기사월간검색
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(329, 9);
+            this.button1.Location = new System.Drawing.Point(800, 8);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 36);
-            this.button1.TabIndex = 3;
+            this.button1.TabIndex = 4;
             this.button1.Text = "검색\r\n(Enter)";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -64,7 +65,7 @@ namespace G1방송기사월간검색
             this.richTextBox1.Location = new System.Drawing.Point(12, 51);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(892, 451);
-            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = "";
             // 
             // comboBox1
@@ -72,16 +73,16 @@ namespace G1방송기사월간검색
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(75, 24);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.Size = new System.Drawing.Size(79, 20);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.Text = "년도";
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(202, 24);
+            this.comboBox2.Location = new System.Drawing.Point(160, 25);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.Size = new System.Drawing.Size(67, 20);
             this.comboBox2.TabIndex = 2;
             this.comboBox2.Text = "월";
             // 
@@ -106,38 +107,46 @@ namespace G1방송기사월간검색
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(200, 9);
+            this.label3.Location = new System.Drawing.Point(158, 10);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(69, 12);
             this.label3.TabIndex = 5;
             this.label3.Text = "검색대상 월";
             // 
-            // label4
+            // search
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(439, 8);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(400, 36);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "이 프로그램은 G1방송 홈페이지에 등록된 기사를 월단위로 검색해줍니다.\r\n기자이름을 적으면 해당 기자가 작성한 기사만 출력됩니다.\r\n전체를 입력하" +
-    "면 해당 월의 전체기사가 출력됩니다.";
+            this.search.Location = new System.Drawing.Point(233, 25);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(198, 21);
+            this.search.TabIndex = 3;
+            this.search.Text = "국회,의원,민주,국민,법,선거";
             // 
-            // Form1
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "다음을 포함(,로 구분)";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(916, 514);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.search);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "G1방송 기사 월간검색 By.신광현";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -154,7 +163,8 @@ namespace G1방송기사월간검색
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox search;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -7,6 +7,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,7 +16,7 @@ namespace G1방송기사월간검색
 {
     public partial class MainForm : Form
     {
-        public static string year = DateTime.Now.ToString("yyyy"), _mon;
+        public static string year = DateTime.Now.ToString("yyyy"), _mon, _search;
         public static int yeari = Convert.ToInt32(year);
 
         public MainForm()
@@ -25,6 +26,7 @@ namespace G1방송기사월간검색
             textBox1.KeyDown += Enter;
             comboBox1.KeyDown += Enter;
             comboBox2.KeyDown += Enter;
+            search.KeyDown += Enter;
 
             for (int i = yeari; i >= 2012; i--)
             {
@@ -40,22 +42,27 @@ namespace G1방송기사월간검색
 
         private void Enter(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == System.Windows.Forms.Keys.Enter) button1_Click(sender,e);
+            if (e.KeyCode == System.Windows.Forms.Keys.Enter) button1_Click(sender, e);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            richTextBox1.ReadOnly = true;
             if (!comboBox1.Text.All(char.IsDigit) || !comboBox2.Text.All(char.IsDigit)) MessageBox.Show("날짜가 숫자가 아닙니다. 다시 시도하세요.", "오류!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             else if (2012 > Convert.ToInt32(comboBox1.Text) || Convert.ToInt32(comboBox1.Text) > yeari) MessageBox.Show("년도는 2012년부터 현재까지 지정 가능합니다.", "년도 오류!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             else if (1 > Convert.ToInt32(comboBox2.Text) || Convert.ToInt32(comboBox2.Text) > 12) MessageBox.Show("월은 1월부터 12월까지 지정 가능합니다.", "월 오류!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            else if (_mon != "" && _mon == comboBox1.Text + comboBox2.Text) MessageBox.Show("직전에 실행한 조건과 동일합니다.", "중복 오류!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (_mon != "" && _mon == comboBox1.Text + comboBox2.Text && _search == search.Text) MessageBox.Show("직전에 실행한 조건과 동일합니다.", "중복 오류!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             else
             {
                 _mon = comboBox1.Text + comboBox2.Text;
+                _search = search.Text;
+                WebClient wc = new WebClient();
+                wc.Encoding = Encoding.UTF8;
+
                 for (int d = 1; d <= DateTime.DaysInMonth(Convert.ToInt32(comboBox1.Text), Convert.ToInt32(comboBox2.Text)); d++)
                 {
                     var date = _mon + d.ToString("00");
-                    Document doc = NSoupClient.Parse(new Uri("https://www.g1tv.co.kr/news/?mid=1_207_6&date=" + date), 5000);
+                    Document doc = NSoupClient.Parse(wc.DownloadString("https://www.g1tv.co.kr/news/?mid=1_207_6&date=" + date));
                     Elements datas = doc.Select("ul.list_box li");
                     int i = 0;
                     foreach (Element data in datas)
@@ -64,9 +71,16 @@ namespace G1방송기사월간검색
                         if (i == 1) continue;
                         else if (data.Select("span.reporter").Text == textBox1.Text + " 기자" || textBox1.Text == "전체")
                         {
-                            richTextBox1.AppendText(date + "\t" + data.Select("a").Attr("title").ToString() + "\t");
-                            richTextBox1.AppendText("https://www.g1tv.co.kr" + data.Select("a").Attr("href").ToString() + "\t");
-                            richTextBox1.AppendText(data.Select("span.reporter").Text + "\n");
+                            foreach (string s in _search.Split(','))
+                            {
+                                if (data.Select("a").Attr("title").ToString().Contains(s))
+                                {
+                                    richTextBox1.AppendText(date + "\t" + data.Select("a").Attr("title").ToString() + "\t");
+                                    richTextBox1.AppendText("https://www.g1tv.co.kr" + data.Select("a").Attr("href").ToString() + "\t");
+                                    richTextBox1.AppendText(data.Select("span.reporter").Text + "\n");
+                                }
+                                continue;
+                            }
                         }
                     }
                 }
